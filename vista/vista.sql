b2efd6b4eed21710f16e0ca9eb37bb78c4d44196
sebastian <109234912+sebastifalck@users.noreply.github.com>
2025-05-27 08:39:40 -0500

    Update vista.sql

--- vista/vista.sql
+++ vista/vista.sql
@@ -12,3 +12,25 @@ JOIN appname_directory adn ON ad.id_appname = adn.id
 JOIN app_type_directory atd ON agp.id_app_type_directory = atd.id
 JOIN env_directory env ON agp.id_env_directory = env.id
 JOIN project_directory pj ON agp.id_project_directory = pj.id;
+
+
+
+CREATE OR REPLACE VIEW view_app_env_versus AS
+SELECT
+    adn.id AS appname_id,
+    adn.app AS app_name,
+    pj.id AS id_project_directory,
+    pj.project_name,
+    
+    MAX(CASE WHEN env.env = 'dev' THEN agp.id END) AS dev_app_general_id,
+    MAX(CASE WHEN env.env = 'qa' THEN agp.id END) AS qa_app_general_id,
+    MAX(CASE WHEN env.env = 'prod' THEN agp.id END) AS prod_app_general_id
+
+FROM app_general_properties agp
+JOIN app_directory ad ON agp.id_app_directory = ad.id
+JOIN appname_directory adn ON ad.id_appname = adn.id
+JOIN project_directory pj ON agp.id_project_directory = pj.id
+JOIN env_directory env ON agp.id_env_directory = env.id
+
+GROUP BY adn.id, adn.app, pj.id, pj.project_name;
+
